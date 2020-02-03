8248f12f674e5a8e7a598a25eb155b1d182c7667
Daniel Gordillo Rodríguez <danielgordillo9@gmail.com>
2020-02-03 02:58:13 +0100

    Casi listo

--- ParejasCartas/ParejasCartas-UI/ViewModels/clsJuegoVM.cs
+++ ParejasCartas/ParejasCartas-UI/ViewModels/clsJuegoVM.cs
@@ -20,8 +20,11 @@ namespace ParejasCartas_UI.ViewModels
         //Un booleano para esp
         //El tiempo no como string poner de formato de en el date solo segundos y min
         private ObservableCollection<clsCarta> _tablero;
-        private string _tiempo;
+        private DispatcherTimer _tiempo;
+        private int _minutos;
+        private int _segundos;
         private string _nombreJugador;
+        private string _resultado;
         private clsCarta _cartaSeleccionada;
         private clsCarta _carta1;
         private clsCarta _carta2;
@@ -31,6 +34,24 @@ namespace ParejasCartas_UI.ViewModels
         {
             clsTablero tablero = new clsTablero();
             _tablero = new ObservableCollection<clsCarta>(tablero.getTablero());
+            _tiempo = new DispatcherTimer();
+            _tiempo.Interval = new TimeSpan(0,0,1);
+            _tiempo.Tick += dispatcherTimer_Tick;
+            _tiempo.Start();
+            _minutos = 0;
+            _segundos = 0;
+        }
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            _segundos++;
+            if(_segundos == 59)
+            {
+                _minutos++;
+                _segundos = 0;
+            }
+            _resultado = $"{_minutos}:{_segundos}";
+            NotifyPropertyChanged("Cronometro");
         }
 
         //Cosas del NotifyPropertyChanged
@@ -74,32 +95,45 @@ namespace ParejasCartas_UI.ViewModels
             set
             {
                 _cartaSeleccionada = value;
+                NotifyPropertyChanged("CartaSeleccionada");
+                if (_cartaSeleccionada.Volteada)
+                {
+                    _cartaSeleccionada = null;
+                }
+                else
+                {
+                    this.comprobarJugada();
+                }
                 
             }
         }
 
-        public int NumeroParejas
+        public string Cronometro
         {
             get
             {
-                return _numeroParejas;
-            }
-            set
-            {
-                _numeroParejas = value;
-                if(_numeroParejas == 6)
-                {
-                    this.mostrarMensajeGanador();
-                }
+                return _resultado;
             }
         }
+
+        //public int NumeroParejas
+        //{
+        //    get
+        //    {
+        //        return _numeroParejas;
+        //    }
+        //    set
+        //    {
+        //        _numeroParejas = value;
+                
+        //}
 
         //Metodos
         /// <summary>
         /// Este metodo comprueba que la pareja es correcta en el caso de que no lo es le da la vuelta a las cartas
         /// Y en el caso de que si sea correcta le suma uno al numero de parejas
         /// </summary>
-        public void comprobarJugada()
+        public async void comprobarJugada()
         {
 
             //Aqui comprobamos que carta es a la que le vamos a dar el valor
@@ -114,61 +148,75 @@ namespace ParejasCartas_UI.ViewModels
                 _carta2 = _cartaSeleccionada;
                 _carta2.Volteada = true;
             }
-
-            //if (this.NumeroParejas == 5)
-            //{
-            //    this.comprobarPareja();
-            //}
-
-            //_carta1Seleccionada = false;
-
-
-            //if (!_carta1Seleccionada)
-            //  {
-            //      if(CartaSeleccionada2 != null)
-            //      {
-            //          this.comprobarPareja();
-            //      }
-
-
-
-            //      if (!_cartaSeleccionada1.Volteada)
-            //      {
-            //          _cartaSeleccionada1.Volteada = true;
-            //          _carta1Seleccionada = true;
-            //      }
-            //      else
-            //      {
-            //          _carta1Seleccionada = false;
-            //      }
-            //  }
-            //  else
-            //  {
-            //      CartaSeleccionada2 = value;
-            //  }
-
-
-            if (CartaSeleccionada.ID == CartaSeleccionada.ID)
+            //Aqui hago la comprobacion de las parejas
+            if (_carta2 != null)
             {
-                NumeroParejas++;
-            }
-            else
-            {
-                CartaSeleccionada.Volteada = false;
-                CartaSeleccionada.Volteada = false;
-            }
-        }
 
-        public async void mostrarMensajeGanador()
+                if (_carta1.ID == _carta2.ID)
+                {
+                    _numeroParejas++;
+                    _carta2 = null;
+                    _carta1 = null;
+
+                }
+                else
+                {
+
+                    //ContentDialog mensaje = new ContentDialog();
+                    //Frame frame = Window.Current.Content as Frame;
+
+                    //mensaje.Title = "Incorrecto";
+                    //mensaje.Content = "Has fallado";
+                    //mensaje.PrimaryButtonText = "Vale?";
+                    Task task = Task.Delay(350);
+                    await task.AsAsyncAction();
+                    _carta1.Volteada = false;
+                    _carta2.Volteada = false;
+                    _carta2 = null;
+                    _carta1 = null;
+                    //mensaje.Hide();
+                    ////ContentDialogResult finale = await mensaje.ShowAsync();
+                    
+
+                }
+
+            }
+
+            if (_numeroParejas == 6)
+            {
+                this.mostrarMensajeGanador();
+            }
+    }
+
+        public async Task<string> mostrarMensajeGanador()
         {
-            ContentDialog mensaje = new ContentDialog();
-            Frame frame = Window.Current.Content as Frame;
+            //    ContentDialog mensaje = new ContentDialog();
+            //    Frame frame = Window.Current.Content as Frame;
 
-            mensaje.Title = "Ganador";
-            mensaje.Content = "Has GANADO";
-            mensaje.PrimaryButtonText = "Bieeennnn";
+            //    mensaje.Title = "Termina";
+            //    mensaje.Content = "Has Terminado";
+            //    mensaje.PrimaryButtonText = "Por fin";
 
-            ContentDialogResult finale = await mensaje.ShowAsync();
+            //    ContentDialogResult finale = await mensaje.ShowAsync();
+
+            string resultado;
+            TextBox inputTextBox = new TextBox();
+            inputTextBox.AcceptsReturn = false;
+            inputTextBox.Height = 32;
+            ContentDialog dialog = new ContentDialog();
+            dialog.Content = inputTextBox;
+            dialog.Title = "Has terminado\n" +
+                "Introduce el nombre";
+            dialog.IsSecondaryButtonEnabled = true;
+            dialog.PrimaryButtonText = "Ok";
+            dialog.SecondaryButtonText = "Cancel";
+            if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+                resultado = inputTextBox.Text;
+            else
+                resultado = "";
+
+            return resultado;
+
         }
 
     }
